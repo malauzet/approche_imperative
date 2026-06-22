b433e69a7565536628ea6f8003a5fb63e562b1fc
malauzet <30903872+malauzet@users.noreply.github.com>
2026-06-22 17:10:21 +0200

    Ajout de l'exercice SommeDeTableauxDiff

---
+++ src/fr/algorithmie/SommeDeTableauxDiff.java
@@ -0,0 +1,27 @@
+package fr.algorithmie;
+
+public class SommeDeTableauxDiff {
+    public static void main(String[] args) {
+
+        int[] tableau1 = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+        int[] tableau2 = {-1, 12, 17, 14, 5, -9, 0, 18 };
+        int[] tableau3 = new int[tableau1.length];
+
+        // Consigne : Créer un tableau somme dont chaque case contient la somme des valeurs situées dans
+        //les cases de même index des autres tableaux. Pour les index qui excède la taille du plus
+        //petit des 2 tableaux on fait comme si la valeur était 0 pour le petit tableau.
+
+        System.out.println("Affichage du tableau :");
+        int index = 0;
+        while (index < tableau2.length) {
+            tableau3[index] = tableau1[index] + tableau2[index];
+            System.out.println(tableau3[index]);
+            index++;
+        }
+        while(index >= tableau2.length &&  index < tableau1.length) {
+            tableau3[index] = tableau1[index];
+            System.out.println(tableau3[index]);
+            index++;
+        }
+    }
+}
