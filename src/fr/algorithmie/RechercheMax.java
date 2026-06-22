8be16aef775c7a419ec83fbd6ad5c73036a44188
malauzet <30903872+malauzet@users.noreply.github.com>
2026-06-22 16:16:25 +0200

    Ajout de l'exercice RechercheMax

---
+++ src/fr/algorithmie/RechercheMax.java
@@ -0,0 +1,20 @@
+package fr.algorithmie;
+
+public class RechercheMax {
+    public static void main(String args[]) {
+
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+
+        // Consigne : Rechercher le plus grand élément du tableau
+        int max = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        // Affichage du plus grand élément trouvé dans le tableau
+        System.out.println("Max: " + max);
+    }
+}
