e57550a34742d182d7fa1bc25391816ff1e93f66
malauzet <30903872+malauzet@users.noreply.github.com>
2026-06-22 21:07:38 +0200

    Ajout de l'exercice FabriquerMur

---
+++ src/fr/algorithmie/FabriquerMur.java
@@ -0,0 +1,41 @@
+package fr.algorithmie;
+
+public class FabriquerMur {
+    public static void main(String[] args) {
+        // Tests de vérification
+        verifier(3, 1, 8, true);
+        verifier(3, 1, 9, false);
+        verifier(3, 2, 10, true);
+        verifier(3, 2, 8, true);
+        verifier(3, 2, 9, false);
+        verifier(6, 1, 11, true);
+        verifier(6, 0, 11, false);
+        verifier(1, 4, 11, true);
+        verifier(0, 3, 10, true);
+        verifier(1, 4, 12, false);
+        verifier(3, 1, 7, true);
+        verifier(1, 1, 7, false);
+    }
+
+    // Consigne : Cette méthode doit produire un algorithme qui retourne s’il est possible ou non de
+    // fabriquer un mur avec des briques de longueur 1 et des briques de longueur 5.
+    static boolean fabriquerMur(int nbSmall, int nbBig, int longueur) {
+        boolean resultat = false;
+
+        int smallB = 1;
+        int bigB = 5;
+
+        int tailleMax = (smallB * nbSmall) + (bigB * nbBig);
+
+        resultat = tailleMax >= longueur;
+
+        return resultat;
+    }
+
+    private static void verifier(int nbSmall, int nbBig, int longueur, boolean b) {
+        if (!fabriquerMur(nbSmall, nbBig, longueur) == b) {
+            System.err.println("Test (" + nbSmall + ", " + nbBig + ", " +
+                    longueur + ") NON passant.");
+        }
+    }
+}
