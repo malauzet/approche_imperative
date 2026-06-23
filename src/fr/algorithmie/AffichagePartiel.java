9adb6032868ae11064621256e4bc732e0194425a
malauzet <30903872+malauzet@users.noreply.github.com>
2026-06-23 11:12:11 +0200

    Modification de l'affichage dans AffichagePartiel

--- src/fr/algorithmie/AffichagePartiel.java
+++ src/fr/algorithmie/AffichagePartiel.java
@@ -1,29 +1,35 @@
 package fr.algorithmie;
 
+import java.util.Arrays;
+
 public class AffichagePartiel {
     public static void main(String[] args) {
 
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 
+        System.out.println("Tableau : " + Arrays.toString(array));
+
+        System.out.println("-------------");
+
         // Consigne : Combiner une boucle et un test de manière à n’afficher que les entiers supérieurs à 3
-        System.out.println("Affichage des élément du tableau supérieurs à 3 :");
+        System.out.print("Affichage des élément du tableau supérieurs à 3 : ");
         for (int i = 0; i < array.length; i++) {
             if (array[i] > 3 ) {
-                System.out.println(array[i] + " est supérieur à 3.");
+                System.out.print(array[i] + " ");
             }
         }
 
-        System.out.println("\n-------------\n");
+        System.out.println("\n-------------");
 
         // Consigne : Combiner une boucle et un test de manière à n’afficher que les entiers pairs
-        System.out.println("Affichage des éléments pairs du tableau :");
+        System.out.print("Affichage des éléments pairs du tableau : ");
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 0) {
-                System.out.println(array[i] + " est un nombre pair.");
+                System.out.print(array[i] + " ");
             }
         }
 
-        System.out.println("\n-------------\n");
+        System.out.println("\n-------------");
 
         // Consigne : Combiner une boucle et un test de manière à n’afficher que valeurs des index pairs
         System.out.println("Affichage des éléments d'index pair du tableau :");
@@ -33,13 +39,13 @@ public class AffichagePartiel {
             }
         }
 
-        System.out.println("\n-------------\n");
+        System.out.println("-------------");
 
         // Consigne : Combiner une boucle et un test de manière à n’afficher que les entiers impairs
-        System.out.println("Affichage des éléments impair du tableau :");
+        System.out.print("Affichage des éléments impair du tableau : ");
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 != 0) {
-                System.out.println(array[i] + " est un nombre impair.");
+                System.out.print(array[i] + " ");
             }
         }
     }
