acd55b35cfbc22f391d00a3c6a01b98bf7006c91
malauzet <30903872+malauzet@users.noreply.github.com>
2026-06-23 11:14:05 +0200

    Modification de l'affichage dans AffichageInverse

--- src/fr/algorithmie/AffichageInverse.java
+++ src/fr/algorithmie/AffichageInverse.java
@@ -6,20 +6,20 @@ public class AffichageInverse {
         int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
 
         // Consigne : Afficher l’ensemble des éléments du tableau grâce à une boucle
-        System.out.println("Affichage des éléments du tableau :");
+        System.out.print("Affichage des éléments du tableau : ");
         for (int i = 0; i < array.length; i++) {
-            System.out.println("index " + i + " : " + array[i]);
+            System.out.print(array[i] + " ");
         }
 
-        System.out.println("\n-------------\n");
+        System.out.println("\n-------------");
 
         // Consigne : Afficher l’ensemble des éléments dans l’ordre inverse du tableau
-        System.out.println("Affichage inversé des éléments du tableau :");
+        System.out.print("Affichage inversé des éléments du tableau : ");
         for (int i = array.length - 1; i >= 0; i--) {
-            System.out.println("Index " + i + " : " + array[i]);
+            System.out.print(array[i] + " ");
         }
 
-        System.out.println("\n-------------\n");
+        System.out.println("\n-------------");
 
         // Consigne : Créer un tableau arrayCopy et copier tous les éléments de array dans arrayCopy
         int[] arrayCopy = new int[array.length];
@@ -28,9 +28,9 @@ public class AffichageInverse {
             arrayCopy[i] = array[i];
         }
 
-        System.out.println("Affichage des éléments de la copie du tableau :");
+        System.out.print("Affichage des éléments de la copie du tableau : ");
         for (int i = 0; i < arrayCopy.length; i++) {
-            System.out.println("index " + i + " : " + arrayCopy[i]);
+            System.out.print(arrayCopy[i] + " ");
         }
     }
 }
