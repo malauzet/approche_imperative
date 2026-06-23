a63ac6c45f2d766a43445d886bf2afb80b90bebc
malauzet <30903872+malauzet@users.noreply.github.com>
2026-06-23 11:01:15 +0200

    Modification de l'affichage dans InteractifChiffresSuivants

--- src/fr/algorithmie/InteractifChiffresSuivants.java
+++ src/fr/algorithmie/InteractifChiffresSuivants.java
@@ -9,9 +9,10 @@ public class InteractifChiffresSuivants {
         // Exemple : Si l’utilisateur saisit 5, le programme affiche : 6, 7, 8, 9, 10, 11, 12, 13, 14, 15.
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Veuillez entrer un nombre entier : ");
+        System.out.print("Veuillez entrer un nombre entier : ");
         int nb = scanner.nextInt();
 
+        System.out.print("Liste des 10 nombres qui suivent " + nb + " : ");
         for (int i = 1; i <= 10; i++) {
             if (i <= 9) System.out.print(nb + i + ", ");
             else System.out.print(nb + i + ".");
